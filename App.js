13ec7bcc6645c5b8661bed1a0b4549ff87db234a
zih-an <zih_an@163.com>
2022-03-07 20:48:01 +0800

    body detection svg bug

--- App.js
+++ App.js
@@ -1,18 +1,129 @@
-import 'react-native-reanimated';
+// import 'react-native-reanimated';
+import Reanimated, {runOnJS} from 'react-native-reanimated';
 import React, {useEffect, useState} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Dimensions } from 'react-native';
 // import InterfaceMovenet from './kotlin';
 import {
   useCameraDevices,
   Camera,
   useFrameProcessor,
 } from 'react-native-vision-camera';
+import Svg, {G, Circle, Polyline, Polygon, Line} from 'react-native-svg';
 
 console.log('in app.js');
 
 export function MoveNet(frame) {
   'worklet';
   return __MoveNet(frame);
+}
+
+function getFrameSize(frameWidth, frameHeight) {
+  let width, height;
+  if (frameWidth > frameHeight && Dimensions.get('window').width>Dimensions.get('window').height){
+    width = frameWidth;
+    height = frameHeight;
+  }else {
+    console.log("Has rotation");
+    width = frameHeight;
+    height = frameWidth;
+  }
+  return [width, height];
+} 
+
+
+function SVGgraph({points, frameWidth, frameHeight}) {
+  // console.log(points);
+  const frameSize = getFrameSize(frameWidth, frameHeight);
+  const viewBox = `0 0 ${frameSize[0]} ${frameSize[1]}`;
+
+  const onePerson = points[0];
+  const r = 10;
+  const circleFill = "#FF9B70";
+  const headcircleFill = "green";
+  const bodycircleFill = "red";
+  const armcircleFill = "blue";
+  const legcircleFill = "yellow";
+  const HeadSVG = () => {
+    return (
+      <G>
+        <G>
+          <Circle class="point4" cx={onePerson[4][0]} cy={onePerson[4][1]} r={r} fill={headcircleFill}/>
+          <Circle class="point2" cx={onePerson[2][0]} cy={onePerson[2][1]} r={r} fill={headcircleFill}/>
+          <Circle class="point0" cx={onePerson[0][0]} cy={onePerson[0][1]} r={r} fill={headcircleFill}/>
+          <Circle class="point1" cx={onePerson[1][0]} cy={onePerson[1][1]} r={r} fill={headcircleFill}/>
+          <Circle class="point3" cx={onePerson[3][0]} cy={onePerson[3][1]} r={r} fill={headcircleFill}/>
+        </G>
+        <Polyline points={``}/>
+      </G>
+    );
+  }
+  const BodySVG = () => {
+    return (
+      <G>
+        <G>
+          <Circle class="point6" cx={onePerson[6][0]} cy={onePerson[6][1]} r={r} fill={bodycircleFill}/>
+          <Circle class="point5" cx={onePerson[5][0]} cy={onePerson[5][1]} r={r} fill={bodycircleFill}/>
+          <Circle class="point11" cx={onePerson[11][0]} cy={onePerson[11][1]} r={r} fill={bodycircleFill}/>
+          <Circle class="point12" cx={onePerson[12][0]} cy={onePerson[12][1]} r={r} fill={bodycircleFill}/>
+        </G>
+        <Polygon points={``}/>
+      </G>
+    );
+  }
+  const ArmSVG = () => {
+    return (
+      <G>
+        <G>
+          <Circle class="point6" cx={onePerson[6][0]} cy={onePerson[6][1]} r={r} fill={armcircleFill}/>
+          <Circle class="point8" cx={onePerson[8][0]} cy={onePerson[8][1]} r={r} fill={armcircleFill}/>
+          <Circle class="point10" cx={onePerson[10][0]} cy={onePerson[10][1]} r={r} fill={armcircleFill}/>
+
+          <Circle class="point5" cx={onePerson[5][0]} cy={onePerson[5][1]} r={r} fill={armcircleFill}/>
+          <Circle class="point7" cx={onePerson[7][0]} cy={onePerson[7][1]} r={r} fill={armcircleFill}/>
+          <Circle class="point9" cx={onePerson[9][0]} cy={onePerson[9][1]} r={r} fill={armcircleFill}/>
+        </G>
+        <Polyline points={``}/>
+        <Polyline points={``}/>
+      </G>
+    );
+  }
+  const LegSVG = () => {
+    return (
+      <G>
+        <G>
+          <Circle class="point12" cx={onePerson[12][0]} cy={onePerson[12][1]} r={r} fill={legcircleFill}/>
+          <Circle class="point14" cx={onePerson[14][0]} cy={onePerson[14][1]} r={r} fill={legcircleFill}/>
+          <Circle class="point16" cx={onePerson[16][0]} cy={onePerson[16][1]} r={r} fill={legcircleFill}/>
+
+          <Circle class="point11" cx={onePerson[11][0]} cy={onePerson[11][1]} r={r} fill={legcircleFill}/>
+          <Circle class="point13" cx={onePerson[13][0]} cy={onePerson[13][1]} r={r} fill={legcircleFill}/>
+          <Circle class="point15" cx={onePerson[15][0]} cy={onePerson[15][1]} r={r} fill={legcircleFill}/>
+        </G>
+        <Polyline d={``}/>
+        <Polyline d={``}/>
+      </G>
+    );
+  }
+  console.log("person0: ", onePerson[0][0], onePerson[0][1]);
+  return (
+    // screen: w 360 h 719.7
+    <Svg width="360" height={719.666} viewBox={viewBox}>
+      {/* <HeadSVG />
+      <BodySVG />
+      <ArmSVG />
+      <LegSVG /> */}
+      <G rotation={-90} translateY={960} origin="0,0">
+          <Circle class="point4" cx={onePerson[4][0]} cy={onePerson[4][1]} r={r} fill={"red"}/>
+          <Circle class="point2" cx={onePerson[2][0]} cy={onePerson[2][1]} r={r} fill={"orange"}/>
+          <Circle class="point0" cx={onePerson[0][0]} cy={onePerson[0][1]} r={r} fill={"yellow"}/>
+          <Circle class="point1" cx={onePerson[1][0]} cy={onePerson[1][1]} r={r} fill={"green"}/>
+          <Circle class="point3" cx={onePerson[3][0]} cy={onePerson[3][1]} r={r} fill={"blue"}/>
+        <Circle cx="0" cy="0" r={10} fill="blue"/>
+        {/* <Line x1={0} y1={0} x2={frameSize[0]} y2={frameSize[1]} stroke="red" strokeWidth="2" /> */}
+        <Line x1={0} y1={0} x2="960" y2="540" stroke="red" strokeWidth="2" />
+      </G>
+    </Svg>
+  );
 }
 
 export default function App() {
@@ -23,11 +134,19 @@ export default function App() {
   const [cameraPermission, setCameraPermission] = useState();
   const [microphonePermission, setMicrophonePermission] = useState();
 
+  // const [frameHeight, setFrameHeight] = useState();
+  // const [frameWidth, setFrameWidth] = useState();
+  const [layoutWidth, setLayoutWidth] = useState();
+  const [layoutHeight, setLayoutHeight] = useState();
+  const [points, setPoints] = useState([new Array(17).fill([0,0])]);
+
+
   const frameProcessor = useFrameProcessor(frame => {
+    // frame: w 960 h 540
     'worklet';
     console.log('[-----------FRAME-----------]');
     let result = MoveNet(frame);
-    console.log('[frame result]: ', result);
+    runOnJS(setPoints)(result);
   }, []);
 
   useEffect(() => {
@@ -50,16 +169,26 @@ export default function App() {
     return <Text>No Device Here</Text>; // no device page...
   }
 
+  const handleCameraLayout = (event) => {
+    const { x, y, width, height } = event.nativeEvent.layout;
+    setLayoutHeight(height);
+    setLayoutWidth(width);
+    console.log("camera size: ", width ,height);
+  };
+
   return (
     <>
       <Camera
         style={styles.cameraStyle}
         device={frontDevice}
         isActive={true}
+        // orientation="portraitUpsideDown"
+        onLayout={handleCameraLayout}
         frameProcessor={frameProcessor}
         frameProcessorFps={1}
       />
-      <Text style={styles.box}>Hi</Text>
+      <SVGgraph points={points} frameWidth={960} frameHeight={540} />
+      {/* <Text style={styles.box}>Hi</Text> */}
     </>
   );
 }
--- android/app/src/main/java/com/rnwithkotlin/MovenetFrameProcessorPlugin.kt
+++ android/app/src/main/java/com/rnwithkotlin/MovenetFrameProcessorPlugin.kt
@@ -1,17 +1,32 @@
 package com.rnwithkotlin
-import android.util.Log
-import androidx.camera.core.ImageProxy
-import com.mrousavy.camera.frameprocessor.FrameProcessorPlugin
+
+import Jama.Matrix
 import android.annotation.SuppressLint
+import android.content.ContentResolver
+import android.content.ContentValues
 import android.content.Context
 import android.graphics.*
+import android.media.Image
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.camera.core.ExperimentalGetImage
+import androidx.camera.core.ImageProxy
 import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.WritableNativeArray
+import com.mrousavy.camera.frameprocessor.FrameProcessorPlugin
 import com.rnwithkotlin.data.Device
 import com.rnwithkotlin.utils.*
-import android.media.Image
-import androidx.camera.core.ExperimentalGetImage
 import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
+import java.lang.Exception
 import java.nio.ByteBuffer
+import java.util.*
 
 
 class MovenetFrameProcessorPlugin(reactContext: ReactApplicationContext): FrameProcessorPlugin("MoveNet") {
@@ -53,18 +68,92 @@ class MovenetFrameProcessorPlugin(reactContext: ReactApplicationContext): FrameP
 
 
     @SuppressLint("UnsafeOptInUsageError")
-    private fun estimatePoses(frame: ImageProxy): Double {
+    private fun estimatePoses(frame: ImageProxy): WritableNativeArray {
+        val screenHeight = frame.height
+        val screenWidth = frame.width
+        Log.d("size: ", "w: ${screenWidth} h: ${screenHeight}")
+
         if (net == null) {
-            return -1.0;
+            return WritableNativeArray();
         }
         val bitmap: Bitmap = toBitmap(frame)
         val ans = net.estimatePoses(bitmap)
-        val tmp: Int = ans.count()
         val matrixList = DataTypeTransfor().listPerson2ListMatrix_Jama(ans)
-        for (i in 0..tmp - 1) {
-            AffineTransprocess().printJama(matrixList.get(i))
+//        val zoomans = frameZoom(matrixList, frame, bitmap)
+//        saveImage(bitmap)
+
+        return ListMatrix2WritableArray(matrixList)
+    }
+
+    //this function will save |
+    private fun saveImage(bitmap:Bitmap) {
+        var fos: OutputStream;
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                val resolver:ContentResolver = context.getContentResolver();
+                val contentValues:ContentValues =  ContentValues();
+                contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, "Image" + ".jpg");
+                contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg");
+                val imageUri: Uri? = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+                fos = Objects.requireNonNull(imageUri)?.let { resolver.openOutputStream(it) }!!;
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos); Objects.requireNonNull(fos);
+            }
+        } catch (e:Exception) {
+            Log.d("error",e.toString());
         }
-        return matrixList.get(0).get(0, 0);
+    }
+
+    private fun frameZoom(obj:List<Matrix>, frame: ImageProxy, bitmap: Bitmap):List<Matrix> {
+        val screenHeight = frame.height
+        val screenWidth = frame.width
+        val imageHeight = bitmap.height
+        val imageWidth = bitmap.width
+
+        val HeightRatio:Double=screenHeight.toDouble()/imageHeight.toDouble()
+        val WidthRatio:Double=screenWidth.toDouble()/imageWidth.toDouble()
+        var afterZoom_obj:MutableList<Matrix> = arrayListOf<Matrix>()
+        val sampleNun=afterZoom_obj.count()
+
+        obj.forEach {
+            var temp:Matrix= Matrix(it.rowDimension,it.columnDimension)
+            for(i in 0..it.rowDimension-1)
+            {
+                for(j in 0..it.columnDimension-1)
+                {
+                    when(j)
+                    {
+                        0->{
+                            temp.set(i,j,temp.get(i,j)*WidthRatio)
+                        }
+                        1->{
+                            temp.set(i,j,temp.get(i,j)*HeightRatio)
+                        }
+                    }
+                }
+            }
+            afterZoom_obj.add(temp)
+        }
+        return afterZoom_obj
+    }
+
+    private fun ListMatrix2WritableArray(obj:List<Matrix>):WritableNativeArray {
+        var obj2:WritableNativeArray = WritableNativeArray()
+        obj.forEach{
+            var temp:WritableNativeArray = WritableNativeArray()
+            val rowNum=it.rowDimension
+            val colNum=it.columnDimension
+            for(i in 0..rowNum-1)
+            {
+                var ttemp:WritableNativeArray = WritableNativeArray()
+                for(j in 0..colNum-1)
+                {
+                    ttemp.pushDouble(it.get(i,j))
+                }
+                temp.pushArray(ttemp)
+            }
+            obj2.pushArray(temp)
+        }
+        return obj2
     }
 
 //    fun shutdown() {
